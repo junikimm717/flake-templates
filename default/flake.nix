2ffb0cca56778b78a4fe3b3e6d8d73646354f378
Juni Kim <68165832+junikimm717@users.noreply.github.com>
2024-08-25 15:53:42 -0400

    Update flake.nix

--- default/flake.nix
+++ default/flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Juni's Vanilla Flake Template";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     flake-utils = {
       url = "github:numtide/flake-utils";
       inputs.nixpkgs.follows = "nixpkgs";
